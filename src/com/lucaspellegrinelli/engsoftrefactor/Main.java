1067f4c3244ff39efac5c9f53b43f2e2f8e355ac
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2020-10-20 20:55:56 -0300

    Commit 8

--- src/com/lucaspellegrinelli/engsoftrefactor/Main.java
+++ src/com/lucaspellegrinelli/engsoftrefactor/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Customer m = new Customer("");
         System.out.println(m.statement());
+        System.out.println(m.htmlStatement());
     }
 }
