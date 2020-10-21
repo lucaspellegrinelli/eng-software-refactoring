62d7dbba7570eb7478d6689e2d56fe5a4cb8e05b
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2020-10-20 21:59:14 -0300

    Commit 15

--- src/com/lucaspellegrinelli/engsoftrefactor/Price.java
+++ src/com/lucaspellegrinelli/engsoftrefactor/Price.java
@@ -5,8 +5,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        int frequentRenterPoints = 1;
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) frequentRenterPoints ++;
-        return frequentRenterPoints;
+        return 1;
     }
 }
